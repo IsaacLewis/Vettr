0c733e64da0a90c6930d5f013bb48c402b0119de
Robert Whitehead <robertwhitehead.07@gmail.com>
2012-04-01 13:52:44 +0100

    lads

--- static/js/employer.js
+++ static/js/employer.js
@@ -1,19 +1,25 @@
 $(function(){
     $.get("api/bookings?skype=" + globalSkype, function(data) {
 
-
-	alert("hello world");
+alert("hello world");
    for( i in data) {
-	var template = $("#skypeCall").html();
-	var html = Mustache.to_html(template, data[i]);
+       
+       data[i].when = Date.parse(data[i].when);
+       var template = $("#skypeCall").html();
+	
+       
+
+       var html = Mustache.to_html(template, data[i]);
 	$('#place').append(html);	    
 	}
 	//$('#tag').attr("src", "http://google.com");
 
+
+
 	$(".con").click(function() {
 	alert("lads");
-	    $("#" + $(this).attr("skype")).html("");
-	alert($(this).attr("skype") + " " + $(this).attr("action") + globalskype);
+	    //$("#" + $(this).attr("skype")).html("");
+	    alert($(this).attr("skype") + " " + $(this).attr("action") + globalskype);
 	    $.get("/api/prospects/contact", function(data) {
 	    });
 	});
